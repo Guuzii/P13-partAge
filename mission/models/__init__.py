e0cd060021f81f8b4f8baa9fd80263b28fa12d9f
Erwan <morioe2809@gmail.com>
2021-03-19 12:49:15 +0100

    add model Mission and create migrations

--- mission/models/__init__.py
+++ mission/models/__init__.py
@@ -1,2 +1,4 @@
 from .mission_status import MissionStatus as _MissionStatus
 from .mission_category import MissionCategory as _MissionCategory
+from .mission_bonus_reward import MissionBonusReward as _MissionBonusReward
+from .mission import Mission as _Mission
---
+++ mission/models/mission.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils.timezone import now
+
+
+class Mission(models.Model):
+    bearer_user = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE, related_name='bearer_user')
+    acceptor_user = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE, related_name='acceptor_user', null=True)
+    status = models.ForeignKey('mission.MissionStatus', on_delete=models.PROTECT)
+    category = models.ForeignKey('mission.MissionCategory', on_delete=models.PROTECT)
+    bonus_reward = models.ForeignKey('mission.MissionBonusReward', on_delete=models.PROTECT)
+    created_at = models.DateTimeField(
+        verbose_name=_('Mission creation date'),
+        default=now
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_('Mission update date'),
+        null=True
+    )
+    title = models.CharField(
+        verbose_name=_('Mission title'),
+        max_length=50
+    )
+    description = models.CharField(
+        verbose_name=_('Mission description'),
+        max_length=255
+    )
+
+    class Meta:
+        verbose_name = _('Mission')
+        verbose_name_plural = _('Missions')
+
+    def __str__(self):
+        return self.title
