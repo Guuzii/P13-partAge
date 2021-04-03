732ce5681c4232d1fecfcd4efe86cd206ec94a24
Erwan <morioe2809@gmail.com>
2021-04-03 14:13:36 +0200

    add shopping models

---
+++ shopping/models/product.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Product(models.Model):
+    label = models.CharField(
+        verbose_name=_('Product label'),
+        max_length=50
+    )
+    price = models.SmallIntegerField(
+        verbose_name=_('Product price')
+    )
+    xp_amount = models.IntegerField(
+        verbose_name=_('Product xp amount'),
+        null=True
+    )
+    path_to_sprite = models.CharField(
+        verbose_name=_('Product path to sprite'),
+        max_length=150,
+        null=True
+    )
+    is_multiple = models.BooleanField(
+        verbose_name=_('Product can be bought multiple times'),
+        default=False
+    )
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    def __str__(self):
+        return self.label
---
+++ shopping/models/product_transaction.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils.timezone import now
+
+
+class ProductTransaction(models.Model):
+    user = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
+    product = models.ForeignKey('shopping.Product', on_delete=models.PROTECT)
+    created_at = models.DateTimeField(
+        verbose_name=_('Mission creation date'),
+        default=now
+    )
+
+    class Meta:
+        verbose_name = _('Product transaction')
+        verbose_name_plural = _('Product transactions')
+
+    def __str__(self):
+        return self.product
